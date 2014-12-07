f99fafbb72ec9deee4c064286978d744df696232
Andres De Leon <agldeleon@gmail.com>
2014-12-06 23:44:35 -0600

    Add edit and delete for team

--- app/Controller/TeamsController.php
+++ app/Controller/TeamsController.php
@@ -6,41 +6,36 @@ class TeamsController extends AppController {
 	public function index() {
       
     }
-	
-	public function add() {
-        $uid = $this->Auth->user('id');
-		$this->set('uid', $uid);
-		if ($this->request->is('post')) {
-            $this->Clase->create();
-            if ($this->Clase->save($this->request->data)) {
-                $this->Session->setFlash(__('Se guardo la informacion correctamente.'));
-                return $this->redirect(array('action' => 'index'));
-            }
-            $this->Session->setFlash(__('No se pudo agregar la informacion.'));
-        }
-    }
-	
-	public function edit($id = null) {
-		if (!$id) {
-			throw new NotFoundException(__('Invalid class'));
-		}
 
-		$clase = $this->Clase->findById($id);
-		if (!$clase) {
-			throw new NotFoundException(__('Invalid class'));
+	
+	public function edit($id = null, $sid = null) {
+		$this->loadModel('Sport');
+		if (!$id) {
+			throw new NotFoundException(__('Opcion invalida.'));
+		}
+		
+		if (!$sid) {
+			throw new NotFoundException(__('Opcion invalida.'));
+		}
+		$sports = $this->Sport->find('all', array(
+            'conditions' => array('id' => $sid)));
+		$this->set('sports', $sports);
+		$team = $this->Team->findById($id);
+		if (!$team) {
+			throw new NotFoundException(__('Opcion invalida.'));
 		}
 
 		if ($this->request->is(array('post', 'put'))) {
-			$this->Clase->id = $id;
-			if ($this->Clase->save($this->request->data)) {
-				$this->Session->setFlash(__('Your post has been updated.'));
-				return $this->redirect(array('action' => 'index'));
+			$this->Team->id = $id;
+			if ($this->Team->save($this->request->data)) {
+				$this->Session->setFlash(__('Se ha guardado exitosamente la informacion.'));
+				return $this->redirect(array('action' => 'index', $sid));
 			}
-			$this->Session->setFlash(__('Unable to update your post.'));
+			$this->Session->setFlash(__('No se pudo actualizar la informacion.'));
 		}
 
 		if (!$this->request->data) {
-			$this->request->data = $clase;
+			$this->request->data = $team;
 		}
 	}
 	
@@ -48,12 +43,11 @@ class TeamsController extends AppController {
 		if ($this->request->is('get')) {
 			throw new MethodNotAllowedException();
 		}
+		
+		$data = array('id' => $id, 'active' => 0);
+		$this->Team->save($data);
 
-		if ($this->Clase->delete($id)) {
-			$this->Session->setFlash(
-				__('Se borro la clase de manera exitosa.')
-			);
-			return $this->redirect(array('action' => 'index'));
-		}
+		return $this->redirect(array('action' => 'index'));
+		$this->Session->setFlash(__('Se ha borrado de manera exitosa.'));
 	}
 }
